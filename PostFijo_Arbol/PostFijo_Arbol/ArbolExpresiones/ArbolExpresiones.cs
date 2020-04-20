abc5e6f1e257b2de8bfd0246007912e73ff8f12b
Ricardo Chian <ricardochianc@gmail.com>
2020-04-20 17:41:38 -0600

    Árbol terminado, falta implementarlo en la consola para probarlo

--- PostFijo_Arbol/PostFijo_Arbol/ArbolExpresiones/ArbolExpresiones.cs
+++ PostFijo_Arbol/PostFijo_Arbol/ArbolExpresiones/ArbolExpresiones.cs
@@ -7,6 +7,7 @@ namespace PostFijo_Arbol.ArbolExpresiones
     public class ArbolExpresiones
     {
         private List<char> Operadores { get; set; }
+        private Nodo Raiz { get; set; }
 
         public ArbolExpresiones()
         {
@@ -16,6 +17,55 @@ namespace PostFijo_Arbol.ArbolExpresiones
             Operadores.Add('|');
             Operadores.Add('.');
             Operadores.Add('+');
+
+            Raiz = null;
+        }
+
+        public void CrearArbol(string expresionPostfija)
+        {
+            var pila = new Stack<Nodo>(); //Pila que guardará momentanemente los nodos para ir formando en orden el árbol
+            var contadorHojas = 1;
+
+            while (expresionPostfija.Length != 0 )
+            {
+                var nuevoNodo = new Nodo();
+
+                if (!Operadores.Contains(expresionPostfija[0]) && expresionPostfija[0] != '#') //Si No es un operador y si NO es el estado de ACEPTACIÓN
+                {
+                    nuevoNodo.ItemExpresion = expresionPostfija[0].ToString();
+                    nuevoNodo.NumNodo = contadorHojas;
+                    nuevoNodo.EsHoja = true;
+                    contadorHojas ++;
+
+                    pila.Push(nuevoNodo);
+                }
+                else if (Operadores.Contains(expresionPostfija[0]) && expresionPostfija[0] != '#' ) //Si ES operador
+                {
+                    if (pila.Count > 2)
+                    {
+                        nuevoNodo.ItemExpresion = expresionPostfija[0].ToString();
+
+                        nuevoNodo.DrchNodo = pila.Pop();
+                        nuevoNodo.IzqNodo = pila.Pop();
+                    }
+                    else
+                    {
+                        nuevoNodo.ItemExpresion = expresionPostfija[0].ToString();
+                        
+                        nuevoNodo.IzqNodo = pila.Pop();
+                    }
+                    pila.Push(nuevoNodo);
+                }
+                else if(expresionPostfija[0] == '#')
+                {
+                    nuevoNodo.ItemExpresion = expresionPostfija[0].ToString();
+
+                    Raiz = pila.Pop();
+                    Raiz.DrchNodo = nuevoNodo;
+                }
+
+                expresionPostfija = expresionPostfija.Remove(0, 1);
+            }
         }
     }
 }
