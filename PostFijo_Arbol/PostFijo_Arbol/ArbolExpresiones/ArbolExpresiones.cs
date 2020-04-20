2180567ec5e6bacd8432ba371a8130ddb332b714
Ricardo Chian <ricardochianc@gmail.com>
2020-04-20 12:24:01 -0600

    Nodo hoja definido

--- PostFijo_Arbol/PostFijo_Arbol/ArbolExpresiones/ArbolExpresiones.cs
+++ PostFijo_Arbol/PostFijo_Arbol/ArbolExpresiones/ArbolExpresiones.cs
@@ -6,6 +6,16 @@ namespace PostFijo_Arbol.ArbolExpresiones
 {
     public class ArbolExpresiones
     {
+        private List<char> Operadores { get; set; }
 
+        public ArbolExpresiones()
+        {
+            Operadores = new List<char>();
+            Operadores.Add('(');
+            Operadores.Add('*');
+            Operadores.Add('|');
+            Operadores.Add('.');
+            Operadores.Add('+');
+        }
     }
 }
--- PostFijo_Arbol/PostFijo_Arbol/ArbolExpresiones/Nodo.cs
+++ PostFijo_Arbol/PostFijo_Arbol/ArbolExpresiones/Nodo.cs
@@ -6,6 +6,28 @@ namespace PostFijo_Arbol.ArbolExpresiones
 {
     public class Nodo
     {
+        public string ItemExpresion { get; set; }
+        public List<string> First { get; set; }
+        public List<string> Last { get; set; }
+        public List<string> Follow { get; set; }
+        public Nodo IzqNodo { get; set; } //Hijo izquierdo
+        public Nodo DrchNodo { get; set; } //Hijo derecho
+        public bool EsHoja { get; set; }
+        public int NumNodo { get; set; } //Servirá para guardar la numeración del nodo en caso de ser hoja
 
+        public Nodo()
+        {
+            ItemExpresion = string.Empty;
+
+            First = new List<string>();
+            Last = new List<string>();
+            Follow = new List<string>();
+
+            IzqNodo = null;
+            DrchNodo = null;
+
+            EsHoja = true;
+            NumNodo = -1;
+        }
     }
 }
